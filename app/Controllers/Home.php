af785853c3bdd9cf9e315879e90aef0f697d701c
wallace-cafe <contato@wallaceo.com>
2026-06-03 18:33:25 -0300

    testando script deploy automatico

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,9 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index()
-    {
-        return view('home');
-    }
+  public function index()
+  {
+    // fazendo um teste de Deploy automático
+    return view('home');
+  }
 }
